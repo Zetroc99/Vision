7608809ca67e28f3581b97e2d34c267dace81616
Zetroc99 <marloncortez_99@hotmail.com>
2022-04-28 11:56:26 -0700

    Vision Update
    Multiple landmark tracking now functioning.
    Code cleaning and documentation in progress.

--- TrackCV.py
+++ TrackCV.py
@@ -5,50 +5,66 @@ import numpy as np
 
 def calculate_angle(a, b, c):
     """
+    Calculates the angle between 3 adjacent landmarks
+    adding up to a maximum angle of 180 degrees.
+    :param a: 2x1 Numpy Array for the first angular point
+    :param b: 2x1 Numpy Array for the second angular point
+    :param c: 2x1 Numpy Array for the third angular point
+    :return: Floating angle value between 0 and 180.
 
-    :param a:
-    :param b:
-    :param c:
-    :return:
+    Examples:
+        >>> a1 = np.array([11,20])
+        >>> b1 = np.array([23,4])
+        >>> c1 = np.array([60,4])
+        >>> calculate_angle(a1,b1,c1)
+        126.86989764584402
+
     """
-    a = np.array(a)  # first
-    b = np.array(b)  # mid
-    c = np.array(c)  # end
-
-    rad = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1],
-                                                            a[0] - b[0])
-    angle = np.abs(rad * 180.0 / np.pi)
-
+    # convert x,y coordinates of a,b,c to radians
+    radian = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1],
+                                                               a[0] - b[0])
+    angle = np.abs(radian * 180.0 / np.pi)
+    # prevents angle from going beyond 180 deg
     if angle > 180.0:
         angle = 360 - angle
     return angle
 
 
-def get_pose_lm():
-    pose = mp.solutions.pose.PoseLandmark.__members__.items()
-    pose_landmarks = [name for name, member in pose]
-    return {k: v for v, k in enumerate(pose_landmarks)}
+def get_coordinates(landmarks, lms):
+    """
+    Gets the interior angles of the tracked landmark(s)
+    and returns them as a Numpy Array
+    :param landmarks:
+    :param lms:
+    :return:
+    """
+
+    coordinates = [[
+        landmarks[lms[i]].x,
+        landmarks[lms[i]].y]
+        for i in range(len(lms))
+    ]
+    return np.array(coordinates)  # returns 3x2 Numpy array
 
 
 class TrackCV:
     """ TrackCV class
-
-    vvvvvvvvvvv
-
+    Will create an instance of body landmarks that need to be
+    tracked in realtime.
     """
+
+    # Instance variables (only applies to holistic solutions atm)
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_holistic = mp.solutions.holistic
 
+    # TODO: change tracked_angles to output {'Left_Arm': [11, 13, 15]}
     def __init__(self, tracked_angles):
-        """Initialize MotionTracking Display
-
         """
-        # self.landmarks = landmarks  # list of strings
-        self.pose_lm = get_pose_lm()  # add hands later
-        # for item in landmarks:
-        #    if item in self.pose_lm:
-        #        self.landmarks.append(item)
+        Record body joint angles that need to be tracked
+        :param tracked_angles: dict consisting of triad of points that will be tracked
+        """
+
         self.tracked_angles = tracked_angles  # dict of center joint and
 
     def track(self):
@@ -58,6 +74,8 @@ class TrackCV:
         """
 
         cap = cv2.VideoCapture(0)  # input should be arg
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         with self.mp_holistic.Holistic(min_detection_confidence=0.8,
                                        min_tracking_confidence=0.8) as holistic:
             while cap.isOpened():
@@ -77,18 +95,19 @@ class TrackCV:
                 # Recolor back to BGR
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 image.flags.writeable = True
+                # TODO: Make tracked_angle a dict that has k: joint and v: list of coordinates
 
-                for lms in self.tracked_angles.values():
-                    try:
-                        p_landmarks = results.pose_landmarks.landmark
+                try:
+                    p_landmarks = results.pose_landmarks.landmark
+                    for lms in self.tracked_angles.values():
                         # get coordinates
-                        lm_coords = self.get_coordinates(p_landmarks, lms)
+                        lm_coords = get_coordinates(p_landmarks, lms)
                         # calculate angle
                         angle = calculate_angle(lm_coords[0],
                                                 lm_coords[1],
                                                 lm_coords[2])
-                        print(angle)
                         # visualize
+                        # visualize is for testing purposes, comment out when not needed
                         cv2.putText(image, str(round(angle, 2)),
                                     tuple(
                                         np.multiply(lm_coords[1], [640, 480]).astype(
@@ -96,8 +115,8 @@ class TrackCV:
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2,
                                     cv2.LINE_AA
                                     )
-                    except:
-                        pass
+                except:
+                    pass
                 # pose
                 self.mp_drawing.draw_landmarks(
                     image,
@@ -107,15 +126,8 @@ class TrackCV:
                         .get_default_pose_landmarks_style())
 
                 cv2.imshow('MediaPipe Holistic', image)
-                if cv2.waitKey(1) == ord('q'):
+                if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
         cap.release()
         cv2.destroyAllWindows()
-
-    def get_coordinates(self, landmarks, lms):  # lm is str
-        coordinates = [[
-            landmarks[getattr(self.mp_holistic.PoseLandmark, lms[i]).value].x,
-            landmarks[getattr(self.mp_holistic.PoseLandmark, lms[i]).value].y]
-            for i in range(3)
-        ]
-        return np.array(coordinates)  # returns 3x2 array
+        cv2.waitKey(1)
