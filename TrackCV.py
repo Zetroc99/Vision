f8930db6386bd56c407e49e02a388920eeeec4a9
Zetroc99 <marloncortez_99@hotmail.com>
2022-05-11 09:07:49 -0700

    Code clean up and adding hands into the mix
    checkpoint

--- TrackCV.py
+++ TrackCV.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import numpy as np
 
 
-def calculate_angle(a, b, c):
+def calculate_angle(a, b, c) -> float:
     """
     Calculates the angle between 3 adjacent landmarks
     adding up to a maximum angle of 180 degrees.
@@ -20,6 +20,9 @@ def calculate_angle(a, b, c):
         126.86989764584402
 
     """
+    assert isinstance(a, np.ndarray), 'a is not a numpy array'
+    assert isinstance(b, np.ndarray), 'b is not a numpy array'
+    assert isinstance(c, np.ndarray), 'c is not a numpy array'
     # convert x,y coordinates of a,b,c to radians
     radian = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1],
                                                                a[0] - b[0])
@@ -30,13 +33,13 @@ def calculate_angle(a, b, c):
     return angle
 
 
-def get_coordinates(landmarks, lms):
+def get_coordinates(landmarks, lms) -> np.ndarray:
     """
-    Gets the interior angles of the tracked landmark(s)
+    Gets the interior angles of three tracked landmarks
     and returns them as a Numpy Array
-    :param landmarks:
-    :param lms:
-    :return:
+    :param landmarks: google.protobuf.pyext._message.RepeatedCompositeContainer w/ size of total landmarks
+    :param lms: list of length 3 with numbers that correspond to the landmark values
+    :return: 3x2 Numpy Array made up of floating values from 0 to 180
     """
 
     coordinates = [[
@@ -45,6 +48,28 @@ def get_coordinates(landmarks, lms):
         for i in range(len(lms))
     ]
     return np.array(coordinates)  # returns 3x2 Numpy array
+
+
+def show_text(image, angle, lm_coords, frame_w, frame_h):
+    cv2.putText(image, str(round(angle, 2)),
+                tuple(
+                    np.multiply(lm_coords[1], [frame_w, frame_h]).astype(
+                        int)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2,
+                cv2.LINE_AA
+                )
+
+
+def calculate_motion(group_landmarks, tracked_group, showText, image, frame_width, frame_height):
+    for lms in tracked_group.values():
+        # get coordinates
+        lm_coords = get_coordinates(group_landmarks, lms)
+        # get angle
+        angle = calculate_angle(lm_coords[0],
+                                lm_coords[1],
+                                lm_coords[2])
+        if showText:
+            show_text(image, angle, lm_coords, frame_width, frame_height)
 
 
 class TrackCV:
@@ -58,26 +83,26 @@ class TrackCV:
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_holistic = mp.solutions.holistic
 
-    # TODO: change tracked_angles to output {'Left_Arm': [11, 13, 15]}
-    def __init__(self, tracked_angles):
+    def __init__(self, tracked_pose=None, tracked_hands=None):  # TODO make sure None args work when None in self.track
         """
         Record body joint angles that need to be tracked
-        :param tracked_angles: dict consisting of triad of points that will be tracked
+        :param tracked_pose: dict consisting of triad of points that will be tracked
         """
 
-        self.tracked_angles = tracked_angles  # dict of center joint and
+        self.tracked_pose = tracked_pose  # dict of center joint and
+        self.tracked_hands = tracked_hands
 
-    def track(self):
+    def track(self, frame_width=640, frame_height=480, min_dc=0.8, max_tc=0.8, showText=False):
         """
 
         :return:
         """
 
-        cap = cv2.VideoCapture(0)  # input should be arg
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        with self.mp_holistic.Holistic(min_detection_confidence=0.8,
-                                       min_tracking_confidence=0.8) as holistic:
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+        with self.mp_holistic.Holistic(min_detection_confidence=min_dc,
+                                       min_tracking_confidence=max_tc) as holistic:
             while cap.isOpened():
                 success, frame = cap.read()  # read video capture
                 if not success:
@@ -95,39 +120,48 @@ class TrackCV:
                 # Recolor back to BGR
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 image.flags.writeable = True
-                # TODO: Make tracked_angle a dict that has k: joint and v: list of coordinates
 
-                try:
+                try:  # TODO add hands / and make function for 'for' loop contents
                     p_landmarks = results.pose_landmarks.landmark
-                    for lms in self.tracked_angles.values():
-                        # get coordinates
-                        lm_coords = get_coordinates(p_landmarks, lms)
-                        # calculate angle
-                        angle = calculate_angle(lm_coords[0],
-                                                lm_coords[1],
-                                                lm_coords[2])
-                        # visualize
-                        # visualize is for testing purposes, comment out when not needed
-                        cv2.putText(image, str(round(angle, 2)),
-                                    tuple(
-                                        np.multiply(lm_coords[1], [640, 480]).astype(
-                                            int)),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2,
-                                    cv2.LINE_AA
-                                    )
+                    lh_landmarks = results.left_hand_landmarks.landmark
+                    #rh_landmarks = results.
+
+                    # calculate pose
+                    calculate_motion(p_landmarks, self.tracked_pose, showText, image, frame_width, frame_height)
+                    # calculate hands
+                    calculate_motion(lh_landmarks, self.tracked_hands, showText, image, frame_width, frame_height)
                 except:
                     pass
-                # pose
+                # draw pose
                 self.mp_drawing.draw_landmarks(
                     image,
                     results.pose_landmarks,
                     self.mp_holistic.POSE_CONNECTIONS,
                     landmark_drawing_spec=self.mp_drawing_styles
                         .get_default_pose_landmarks_style())
-
+                # TODO add hands
+                # draw hands
+                self.mp_drawing.draw_landmarks(
+                    image,
+                    results.left_hand_landmarks,
+                    self.mp_holistic.HAND_CONNECTIONS,
+                    landmark_drawing_spec=self.mp_drawing_styles
+                        .get_default_hand_landmarks_style())
                 cv2.imshow('MediaPipe Holistic', image)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
         cap.release()
         cv2.destroyAllWindows()
         cv2.waitKey(1)
+
+    def get_tracked_angles(self, pose=True):
+        """
+        Getter for tracked angle names/key values
+        :return: list of tracked angle names
+        """
+        if pose:
+            return list(self.tracked_pose.keys())
+        else:
+            return list(self.tracked_hands.keys())
+
+    # TODO possible setter angles, in case more want to be added
