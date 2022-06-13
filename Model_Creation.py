2c36c15bd54dcb2356c5a7bef1a3bb09cfe0bbc9
Zetroc99 <49223290+Zetroc99@users.noreply.github.com>
2022-06-13 14:53:31 -0700

    model creation image aquisition
    Can now save images, fine tuning is needed now

--- Model_Creation.py
+++ Model_Creation.py
@@ -1,69 +1,82 @@
 import cv2 as cv
-import mediapipe as mp
 import numpy as np
+import copy
 import time
+import mediapipe as mp
 
 COUNT = 1
 MAX_IMAGES = 50
 
+
+def get_bbox_coordinates(landmarks, width, height):
+    """
+    """
+    all_x, all_y = [], []  # store all x and y points in list
+    for lm in landmarks:
+        all_x.append(int(lm.x * width))  # multiply x by image width
+        all_y.append(int(lm.y * height))  # multiply y by image height
+
+    return min(all_x), min(all_y), max(all_x), max(
+        all_y)  # return as (xmin, ymin, xmax, ymax)
+
+
 cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-cap.set(3, 640)
-cap.set(4, 640)
+w = 1280
+h = 600
+cap.set(3, w)
+cap.set(4, h)
 
-while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-        print("Ignoring empty camera frame.")
-        # If loading a video, use 'break' instead of 'continue'.
-        break
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
 
-    cv.imshow('Test', image)
+with mp_holistic.Holistic(min_detection_confidence=0.8,
+                          min_tracking_confidence=0.8) as holistic:
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            break
 
-    if cv.waitKey(1) == 32 and COUNT < 3:
-        cv.imwrite('model_img/image_{}.png'.format(COUNT), image)
-        print('Frame {} saved successfully'.format(COUNT))
-        COUNT += 1
+        results = holistic.process(frame)
+        frame.flags.writeable = True
+        box = frame
+        saved_img = frame
 
-    if COUNT == MAX_IMAGES:
-        break
+        try:
+            hand_landmarks = results.right_hand_landmarks.landmark
+        except:
+            pass
+        else:
+            x_min, y_min, x_max, y_max = get_bbox_coordinates(hand_landmarks,
+                                                              w, h)
+            x_min = x_min - 50
+            x_max = x_max + 50
+            y_min = y_min - 50
+            y_max = y_max + 100
+            points = np.array([[x_min, y_min], [x_max, y_min],
+                               [x_max, y_max], [x_min, y_max]])
 
-cap.release()
-cv.destroyAllWindows()
-cv.waitKey(1)
+        try:
+            mask = np.zeros(frame.shape, np.uint8)
+            cv.drawContours(mask, [points], -1, (255, 255, 255), -1, cv.LINE_AA)
+        except:
+            pass
+        else:
+            box = cv.bitwise_and(frame, mask)
+            saved_img = frame[y_min:y_max, x_min:x_max]
+            cv.imshow('Saved Image', cv.flip(saved_img, 1))
+            cv.imshow('Box Image', cv.flip(box, 1))
 
-import cv2 as cv
-import numpy as np
-import copy
+        if cv.waitKey(1) == 32 and COUNT < 3:
+            cv.imwrite('model_img/image_{}.png'.format(COUNT), saved_img)
+            print('Frame {} saved successfully'.format(COUNT))
+            COUNT += 1
 
-# SOME CODE FOR JUMPING WITH HAND TRACKING
-    # IF RIGHT HAND IN FIST IS RAISED:
-        # PRESS SPACE BAR
+        if COUNT == MAX_IMAGES:
+            break
 
-points = np.array([[500, 500], [1000, 500], [1000, 1000],  [500, 1000]])  # points will come from detection rectangle
-(x, y, w, h) = cv.boundingRect(points)
-
-points = points - points.min(axis=0)
-
-# Initialize the webcam
-cap = cv.VideoCapture(0)
-
-while True:
-    # Read each frame from the webcam
-    success, frame = cap.read()
-    gray_img = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
-    blur = cv.GaussianBlur(gray_img, (41, 41), 0)
-    ret, thresh = cv.threshold(blur, 100, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
-    mask = np.zeros(frame.shape, np.uint8)
-    cv.drawContours(mask, [points], -1, (255, 255, 255), -1, cv.LINE_AA)
-    result = cv.bitwise_and(frame, mask)
-
-    cv.imshow("Output", thresh)
-
-    if cv.waitKey(1) == ord('q'):
-        break
-
-# release the webcam and destroy all active windows
 cap.release()
 cv.destroyAllWindows()
 cv.waitKey(1)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+# THIS FILE IS MORE OF A TESTING GROUND FOR NEW IDEAS
+
 import cv2 as cv
 import mediapipe as mp
-# import pyautogui
-# import pydirectinput
-# import numpy as np
-# import time
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -22,43 +21,55 @@ def get_bbox_coordinates(landmarks, width, height):
         all_y)  # return as (xmin, ymin, xmax, ymax)
 
 
-cap = cv.VideoCapture(0)
-cap.set(3, 2560)
-cap.set(4, 1440)
-w = 2560
-h = 1440
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+w = 1280
+h = 600
+cap.set(3, w)
+cap.set(4, h)
 
+# creates box around hands
 with mp_holistic.Holistic(min_detection_confidence=0.8,
                           min_tracking_confidence=0.8) as holistic:
-    #_, frame = cap.read()
-    #h, w, c = frame.shape
-
     while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            break
+        success, frame = cap.read()
+        # gray_img = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+        # blur = cv.GaussianBlur(gray_img, (41, 41), 0)
+        # ret, thresh = cv.threshold(blur, 100, 255,
+        #                           cv.THRESH_BINARY + cv.THRESH_OTSU)
 
-        results = holistic.process(image)
-        image.flags.writeable = True
+        results = holistic.process(frame)
+        frame.flags.writeable = True
+        box = frame
         try:
             hand_landmarks = results.right_hand_landmarks.landmark
 
             x_min, y_min, x_max, y_max = get_bbox_coordinates(hand_landmarks,
                                                               w, h)
-            cv.rectangle(image, (x_min-100, y_min-100), (x_max+100, y_max+100),
-                         (0, 255, 0), 2)
+            # shows highlighted box around the ROI
+            # cv.rectangle(frame, (x_min - 50, y_min - 50),
+            #             (x_max + 50, y_max + 100), (0, 255, 0), 2)
+            points = np.array(
+                [[x_min - 50, y_min - 50], [x_max + 50, y_min - 50],
+                 [x_max + 50, y_max + 100], [x_min - 50, y_max + 100]])
 
         except:
             pass
+        try:
+            mask = np.zeros(frame.shape, np.uint8)
+            cv.drawContours(mask, [points], -1, (255, 255, 255), -1, cv.LINE_AA)
+        except:
+            pass
+        else:
+            box = cv.bitwise_and(frame, mask)
+
         mp_drawing.draw_landmarks(
-            image,
+            frame,
             results.right_hand_landmarks,
             mp_holistic.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-        cv.imshow('Test', cv.flip(image,1))
+
+        cv.imshow('Test', cv.flip(box, 1))
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
